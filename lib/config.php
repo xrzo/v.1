deaee8626e418c6be2881278c76e29f4bf08df44
pedjazyzz <68003615+pedjazyzz@users.noreply.github.com>
2020-07-09 13:43:17 +0200

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -9,7 +9,7 @@ Jika akun terkena feedback_required tenang, tinggal tunggu 24 jam ntar pulih lag
 */
 
 //UBAH BAGIAN INI
-$countTarget    = '15'; //Ambil jumlah akun per target
+$countTarget    = '20'; //Ambil jumlah akun per target
 $sleep_1        = '5'; //Jeda per view story
 $sleep_2        = '5'; //Jeda per view story 1 akun user
 //SAMPAI SINI AJA
